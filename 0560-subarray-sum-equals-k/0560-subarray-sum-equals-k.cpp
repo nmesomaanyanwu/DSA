dfa33832ee3b0f998d78fa702a4be14e064a23dd
Nmesoma Anyanwu <150448018+nmesomaanyanwu@users.noreply.github.com>
2026-08-24 14:15:42 +0100

    Time: 47 ms (48.07%), Space: 45.6 MB (39.26%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,34 +2,27 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         if (nums.empty()){
-            return 0;
+        return 0;
         }
+
+        unordered_map<int , int> freq = {{0, 1}};
+
+    
+        int prefix_sum = 0;
         int count = 0;
-        
 
-        vector<int> prefix_sum = {0};
         for (auto num : nums){
-            int prev = prefix_sum.back();
-            prefix_sum.push_back(num + prev);
-        }
+            prefix_sum += num;
 
-        int n = prefix_sum.size();
+            int needed = prefix_sum - k;
 
-
-        for (int i = 0 ; i < n ;i++){
-            int c = 0 ;
-
-            while (c < i){
-                if (prefix_sum[i] - prefix_sum[c] == k){
-                    count++;
-                }
-                
-                c++; 
+            if (freq.count(needed) == 1){
+                count += freq[needed];
             }
-        }
 
+            freq[prefix_sum]++;
+        }
 
         return count;
-        
     }
 };
