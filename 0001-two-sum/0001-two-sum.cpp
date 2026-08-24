a25d8a0c9233a9104bf7f7bdc72104d36d950b91
Nmesoma Anyanwu <150448018+nmesomaanyanwu@users.noreply.github.com>
2026-08-24 11:13:04 +0100

    Time: 0 ms (100%), Space: 14.9 MB (25.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         /*firstly keep a map of nums weve seen if target - current is in map return the indices of that nand this*/
-        map<int , int> seen;
+        unordered_map<int , int> seen;
 
         for (int i = 0 ; i < nums.size(); i++){
             int diff = target - nums[i];
